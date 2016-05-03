3939fae2af0e86ec07de74ea15da716935bfcdae
Allie Sudholt <allison.sudholt@gmail.com>
2016-05-03 16:54:58 -0500

    sessions working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,11 @@ class BooksController < ApplicationController
   end
 
   def selected
-      url = 'https://www.googleapis.com/books/v1/volumes?filter=partial&maxResults=1&langRestrict=en&printType=books&projection=lite&q=' + params[:selected].to_s + '+subject%20:' + params[:option].to_s + '&startIndex=' + rand(50).to_s
+    # debugger
+      url = 'https://www.googleapis.com/books/v1/volumes?filter=partial&maxResults=1&langRestrict=en&printType=books&q=' + session[:type].to_s + '+subject%20:' + session[:genre].to_s + '+subject%20:' + session[:topic] + '&startIndex=' + rand(50).to_s
       response = HTTParty.get(url)
       @recommendation = response.parsed_response
+
 
   end
 
@@ -25,7 +27,6 @@ class BooksController < ApplicationController
   end
 
   def update
-
     if params[:id].to_i < 3
       @next_id = params[:id].to_i + 1
     else
@@ -35,17 +36,24 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @selected_option = params[:option]
 
-      redirect_to controller: 'books', action: 'show',  id: @next_id, option: params[:option], selected: params[:selected]
+    if params[:id] == "1"
+      session[:type] = params[:option]
+    elsif params[:id] == "2"
+      session[:genre] = params[:option]
+    elsif params[:id] =="3"
+      session[:topic] = params[:option]
+    end
 
-    # if params[:id] = '2'
-    #   @selected_option = @option1
-    # elsif params[:id] = '3'
-    #   @selected_option = @option2
-    # elsif params[:id] = '1'
-    #   @selected_option = @option3
+    redirect_to controller: 'books', action: 'show',  id: @next_id
+
+    # if params[:id] == "2"
+    #   @option1 = params[:option]
+    # elsif params[:id] == "3"
+    #   @option2 = params[:option]
     # end
-      # book_path(params.merge(option: @selected_option))
 
+      # redirect_to controller: 'books', action: 'show',  id: @next_id, option: params[:option], selected1: @option1, selected2: @option2
+      # book_path(params.merge(option: @selected_option))
   end
 
 end
