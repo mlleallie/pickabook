302b5bc80932151b6df86a6e85d376694e61ab0c
Allie Sudholt <allison.sudholt@gmail.com>
2016-05-04 14:49:50 -0500

    api functionality in place

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,18 +3,56 @@ class BooksController < ApplicationController
   end
 
   def selected
-    # debugger
-      url = 'https://www.googleapis.com/books/v1/volumes?filter=partial&maxResults=1&langRestrict=en&printType=books&q=' + session[:type].to_s + '+subject%20:' + session[:genre].to_s + '+subject%20:' + session[:topic] + '&startIndex=' + rand(50).to_s
+    #google books JSON api request
+      url = 'https://www.googleapis.com/books/v1/volumes?filter=partial&maxResults=1&langRestrict=en&printType=books&q=' + session[:type].to_s + '+subject%20:' + session[:genre].to_s + '+subject%20:' + session[:topic] + '&startIndex=' + rand(20).to_s
       response = HTTParty.get(url)
-      @recommendation = response.parsed_response
 
+      #variables to be used from google books api
+        begin
+          @title = response['items'][0]['volumeInfo']['title']
+        rescue
+          @title = ' '
+        end
 
+        begin
+          @author = response['items'][0]['volumeInfo']['authors'][0]
+        rescue
+          @author = ' '
+        end
+
+        begin
+          @description = response['items'][0]['volumeInfo']['description']
+        rescue
+          @description = nil
+        end
+
+        begin
+          @imageURL = response['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+        rescue
+          @imageURL = nil
+        end
+
+        begin
+          @id = response['items'][0]['id']
+        rescue
+          @id = nil
+        end
+
+        @recommendation = response.parsed_response
+
+      #goodreads API request
+        response1 = HTTParty.get('https://www.goodreads.com/book/title.xml?author=' + @author + '&key=' + DB_CONN_GOODREADS_KEY + '&title=' + @title)
+        data = response1.parsed_response
+        # @data = data
+        @averageRating = data['GoodreadsResponse']['book']['average_rating']
+        @url = data['GoodreadsResponse']['book']['url']
+        @isbn = data['GoodreadsResponse']['book']['isbn']
   end
 
   def show
     @book = Book.find(params[:id])
 
-    options_hash = {1 =>["fiction", "non-fiction", "biography"], 2 => ["humor", "drama", "satire", "crime", "fantasty", "sci-fi", "tragedy", "horror", "mystery"], 3 => ["cats", "feminism", "pants", "aliens", "weather"]}
+    options_hash = {1 =>["fiction", "non-fiction", "biography"], 2 => ["humor", "drama", "satire", "crime", "tragedy", "horror", "mystery", "romance"], 3 => ["history", "feminism", "cats", "outerspace", "time travel", "simplicity", "faith", "coming of age", "travel", "exercise", "love", "serial killer", "war", "adventure", "pirates"]}
 
     @options = options_hash[params[:id].to_i]
 
